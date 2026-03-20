36fbedc7a9d571f108a795520cba9a26473519c8
Rajnish Anand <rajnishanand333@gmail.com>
2026-03-20 11:49:12 +0530

    update

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = tfc-demo-staging-bucket-02
+  bucket = "tfc-demo-staging-bucket-02"
 
   tags = merge(local.common_tags, {
     Name = "S3 Bucket - ${var.bucket_name}"
